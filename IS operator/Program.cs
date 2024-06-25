d0c4c96448728bb8a43837989e6b76dfc29729b3
RockingWithTheBest <151398987+RockingWithTheBest@users.noreply.github.com>
2024-06-25 21:35:58 +0300

    ade

---
+++ IS operator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object num = 0;
+            num = "string";
+            bool check = num is int;
+            Console.WriteLine(check);
+        }
+    }
+}
